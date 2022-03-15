21b44516fc8d8a19c53ee4162848cd7dcffe414e
visionteacher <101485461+visionteacher@users.noreply.github.com>
2022-03-15 19:20:43 -0400

    Update CrossplayPlugin.cs

--- Crossplay/CrossplayPlugin.cs
+++ Crossplay/CrossplayPlugin.cs
@@ -24,7 +24,7 @@ namespace Crossplay
         public override string Name => "Crossplay";
         public override string Author => "Moneylover3246";
         public override string Description => "Enables crossplay for terraria";
-        public override Version Version => new Version("1.10.2");
+        public override Version Version => new Version("1.10.3");
 
         public static CrossplayConfig Config = new CrossplayConfig();
 
